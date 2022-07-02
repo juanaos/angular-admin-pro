68146cf10e1c45165b9fd14f4c3888e6137a04cc
juanaos <juan.aos@ecovium.com>
2022-07-02 10:42:48 +0100

    Esqueleto Admin Pro

---
+++ src/app/pages/graphisc1/graphisc1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graphisc1',
+  templateUrl: './graphisc1.component.html',
+  styles: [
+  ]
+})
+export class Graphisc1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
